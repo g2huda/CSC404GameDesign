2d201534f4c9bb5859b1ff9c9709307e899079c8
g2huda <huda.fouda@mail.utoronto.ca>
2018-01-04 12:42:27 -0500

    files for future updates

---
+++ Tandem/Assets/Scripts/CommandPattern/Command.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CommandPattern
+{
+    public abstract class Command
+    {
+        public abstract void exec();
+    }
+}
---
+++ Tandem/Assets/Scripts/CommandPattern/MoveForward.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace CommandPattern
+{
+    public class MoveForward : Command
+    {
+        public override void exec()
+        {
+            
+        }
+    }
+}
